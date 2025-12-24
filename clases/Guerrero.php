6abbc329ae392492fad5eebdfaf5b81852214ff3
DESKTOP-O9QENCI\Arnau <arnauferrer67@gmail.com>
2025-12-24 16:07:41 +0100

    Guerrero listo

---
+++ clases/Guerrero.php
@@ -0,0 +1,31 @@
+<?php
+
+class Guerrero extends Personaje {
+
+    private $espada;
+
+    function __construct ($nombre, $fuerza, $espada) {
+        $this -> espada = $espada;
+
+        parent::__construct ($nombre, $fuerza);
+        
+    }
+
+    public function atacar() {
+
+        $probabilidad = rand(0,1);
+
+        if ($probabilidad = 1) {
+            
+            parent::atacar();
+            parent::atacar();
+        
+        } else {
+
+            echo $this -> nombre . " ha fallado el golpe <br>";
+
+        }
+
+    }
+
+}
