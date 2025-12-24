96c3be09e92eb49c45cd63c1a9b0888e6f52fbc8
DESKTOP-O9QENCI\Arnau <arnauferrer67@gmail.com>
2025-12-24 16:32:25 +0100

    Preparación lista, ahora se empezará la función 1

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     $combatientes = [
 
         new Guerrero ('Arken de Ferroclau', 2, 'Llumferma'),
-        new Guerrero ('', 1, 'Trencasilencis')
+        new Guerrero ('Braloc Tallvent', 1, 'Trencasilencis'),
+        new Mago ('Elyndar Llumdarc', 3),
+        new Mago ('Maelis d’Argentí', 2),
+        new Mago ('Vorhen el Silenciós', 1)
 
     ];
 
